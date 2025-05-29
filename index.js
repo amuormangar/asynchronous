6e7e7cec80fc088b24458c41be11da92e5d040d9
Amuor <mangaramuor@gmail.com>
2025-05-29 18:08:26 +0300

    Asynchronous practice questions

--- index.js
+++ index.js
@@ -24,26 +24,35 @@ users.forEach(email =>{
 })
 
 // You want to simulate a login system that tries to log in a user. The first two attempts fail, but the third succeeds. Write a function tryLogin() that uses a counter and Promises. Use setTimeout to simulate a 1-second delay between attempts. Log "Login successful" or "Login failed after 3 attempts" based on whether login succeeds.
+// pseudocode
+// create a function call tryLogin()
+// Initialize a variable to store the count at 0
+// define an inner function 
+// create a promise to resolve the third login 
+// create setTimeout and increment the count 
+// use a if condition to log attempts less than 3 and print out a message 
+// if attempt is equal to 3 ,it should be a successful login 
 
 
  function tryLogin(){
-    let attempts = 1;
- return new Promise((reject,resolve) =>{
+    let attempts = 0;
+ return new Promise(resolve=>{
  const  loginAttempt = ()=>{
     attempts++;
-    console.log(`${attempts}`)
     setTimeout(()=>{
         if(attempts < 3){
-            reject("Login attempt fail")
+            console.log("Login failed,try again")
             loginAttempt();
         }else{
-            resolve("Login successfully");
+            console.log("Successful login")
+            resolve();
         }
     },1000)
  };
 loginAttempt();
  })
 }
+tryLogin();
 
 
 
@@ -76,7 +85,12 @@ console.log("Time is up!")
 
 
 // You are simulating a page that loads data in stages. Create an async function called loadPage() that: logs "Loading header...", waits 1 second; logs "Loading content...", waits 2 seconds; logs "Loading footer...", waits 1 second; and finally logs "Page fully loaded". Use setTimeout inside Promises and await them in sequence.
-pseudocode
+// pseudocode
+// create a function that takes data as a parameter 
+// create a promise to resolve the data and return .
+// create a setTimeout to stimulate the loading 
+// create async function call loadPage 
+
 
 
 function pageLoading(data){
@@ -98,8 +112,12 @@ loadPage();
 
 // You are simulating fetching stock prices with delays. Write a function fetchPrice(symbol) that returns a Promise which resolves after 2 seconds with the message "Price for [symbol] retrieved". Use async/await to call it for two different stocks ("AAPL" and "GOOG") and log the messages in order.
 //  pseudocode
-//  Define a function call fetchPrice with a parameter Symbol
-//  Create a new promise  and resolve after 2 seconds
+//  Define a function call fetchPrice with a parameter Symbol.
+//  Create a new promise  and resolve after 2 seconds.
+// create a setTimeout to stimulates delays between the prices given 
+//   use a async keyword before the function to return the promise 
+//    use await keyword to pause the execution of the prices 
+
 
 
  function fetchPrice(symbol){
@@ -109,11 +127,16 @@ loadPage();
         },2000)
     })
  }
+ fetchPrice();
+
  async function fetchStockPrices(){
     const AAPLPrices = await fetchPrice("AAPL")
     console.log("AAPLPrices")
     const pricesGooG = await fetchPrice ("GooG")
+    console.log("priceGooG")
  }
+
+ fetchStockPrices();
 
 
 
